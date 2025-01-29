29766289996109316bb4ec6f16a505775589236d
pbusenius <14203989+pbusenius@users.noreply.github.com>
2025-01-29 21:46:07 +0100

    implemente simple ws iterator for the aisstream source

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"os"
 
-	aisstream "github.com/aisstream/ais-message-models/golang/aisStream"
-	"github.com/gorilla/websocket"
+	"github.com/DataDrivenPortIndex/ais-source/internal/source"
 	"github.com/joho/godotenv"
 )
 
@@ -17,47 +14,13 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	url := "wss://stream.aisstream.io/v0/stream"
-	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
+	aisSource, err := source.NewAisStreamSource()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
-	defer ws.Close()
+	defer aisSource.Close()
 
-	subMsg := aisstream.SubscriptionMessage{
-		APIKey:        os.Getenv("API_TOKEN"),
-		BoundingBoxes: [][][]float64{{{-90.0, -180.0}, {90.0, 180.0}}}, // bounding box for the entire world
-	}
-
-	subMsgBytes, _ := json.Marshal(subMsg)
-	if err := ws.WriteMessage(websocket.TextMessage, subMsgBytes); err != nil {
-		log.Fatalln(err)
-	}
-
-	for {
-		_, p, err := ws.ReadMessage()
-		if err != nil {
-			log.Fatalln(err)
-		}
-		var packet aisstream.AisStreamMessage
-
-		err = json.Unmarshal(p, &packet)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		var shipName string
-		// field may or may not be populated
-		if packetShipName, ok := packet.MetaData["ShipName"]; ok {
-			shipName = packetShipName.(string)
-		}
-
-		switch packet.MessageType {
-		case aisstream.POSITION_REPORT:
-			var positionReport aisstream.PositionReport
-			positionReport = *packet.Message.PositionReport
-			fmt.Printf("MMSI: %d Ship Name: %s Latitude: %f Longitude: %f\n",
-				positionReport.UserID, shipName, positionReport.Latitude, positionReport.Longitude)
-		}
+	for aisMessage := range aisSource.Read() {
+		fmt.Println(aisMessage)
 	}
 }
--- internal/source/aisstream.go
+++ internal/source/aisstream.go
@@ -1,6 +1,70 @@
 package source
 
-type AisStreamSource interface {
+import (
+	"encoding/json"
+	"iter"
+	"log"
+	"os"
+
+	aisstream "github.com/aisstream/ais-message-models/golang/aisStream"
+	"github.com/gorilla/websocket"
+)
+
+type AisStreamSource struct {
 	// TODO: add iterator
-	Read()
+	ws *websocket.Conn
+}
+
+func NewAisStreamSource() (*AisStreamSource, error) {
+	url := "wss://stream.aisstream.io/v0/stream"
+	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	subMsg := aisstream.SubscriptionMessage{
+		APIKey:        os.Getenv("API_TOKEN"),
+		BoundingBoxes: [][][]float64{{{-90.0, -180.0}, {90.0, 180.0}}}, // bounding box for the entire world
+	}
+
+	subMsgBytes, _ := json.Marshal(subMsg)
+	if err := ws.WriteMessage(websocket.TextMessage, subMsgBytes); err != nil {
+		return nil, err
+	}
+
+	return &AisStreamSource{
+		ws: ws,
+	}, nil
+}
+
+func (s *AisStreamSource) Close() {
+	s.ws.Close()
+}
+
+func (s *AisStreamSource) Read() iter.Seq[aisstream.AisStreamMessage] {
+	return func(yield func(aisstream.AisStreamMessage) bool) {
+		// items := []model.AisMessage{}
+		// for _, v := range items {
+		// 	if !yield(v) {
+		// 		return
+		// 	}
+		// }
+
+		for i := 0; i < 1000; i++ {
+			_, p, err := s.ws.ReadMessage()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			var packet aisstream.AisStreamMessage
+
+			err = json.Unmarshal(p, &packet)
+			if err != nil {
+				return
+			}
+
+			if !yield(packet) {
+				return
+			}
+		}
+	}
 }
